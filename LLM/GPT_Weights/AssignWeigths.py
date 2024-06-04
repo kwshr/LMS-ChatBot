d1d43e83efa4b0dd7bb1e538744370c831390dfb
Kawshar <kwshr96626@gmail.com>
2024-06-03 23:11:58 -0400

    Add new file & refactor

---
+++ LLM/GPT_Weights/AssignWeigths.py
@@ -0,0 +1,6 @@
+import torch
+
+def assign(left, right):
+    if left.shape != right.shape:
+        raise ValueError(f"Shape mismatch. Left: {left.shape}, Right: {right.shape}")
+    return torch.nn.Parameter(torch.tensor(right))
