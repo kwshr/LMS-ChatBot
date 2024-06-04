34331cd412a6341a6b4be041360dcfd1de38ee9b
Kawshar <kwshr96626@gmail.com>
2024-06-04 11:36:51 -0400

    Refactor

--- LLM/Train/TrainLoaderConfig.py
+++ LLM/Train/TrainLoaderConfig.py
@@ -1,6 +1,6 @@
 import torch
 import sys
-sys.path.insert(0,'C:\dev\LMS-ChatBot\LLM\Data')
+sys.path.insert(0,'C:\dev\LMS-ChatBot\LLM\Data_Objects')
 from DataLoader import create_dataloader_v1
 from GPTConfig import GPT_CONFIG_124M
 from Trainingdata import train_data
