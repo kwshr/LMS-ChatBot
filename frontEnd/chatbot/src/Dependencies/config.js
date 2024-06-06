b95be4b390dc278ecb529101cf854c7e4bcf89fc
Kawshar <kwshr96626@gmail.com>
2024-06-06 17:51:13 -0400

    Update Chatbots UI

--- frontEnd/chatbot/src/Dependencies/config.js
+++ frontEnd/chatbot/src/Dependencies/config.js
@@ -1,7 +1,34 @@
 import { createChatBotMessage } from 'react-chatbot-kit';
+import ChatbotAvatar from '../UI/BotAvatar.js';
+import UserAvatarIcon from '../UI/UserAvatar.js'
+
+const botName = 'YorkCourseWizard';
 
 const config = {
-  initialMessages: [createChatBotMessage(`Hello world`)],
+  initialMessages: [createChatBotMessage(`Hi! I'm ${botName}`)],
+  customStyles: {
+    botMessageBox: {
+      backgroundColor: 'red',
+    },
+    chatButton: {
+      backgroundColor: 'red',
+    },
+  },
+
+  customComponents: {
+    // Replaces the default header
+   header: () => <div style={{ backgroundColor: 'red', padding: "5px", borderRadius: "3px" , display: "ruby-text"}}>YorkCourseWizard</div>,
+   // Replaces the default bot avatar
+   botAvatar: (props) => <ChatbotAvatar {...props} />,
+   // Replaces the default user icon
+   userAvatar: (props) => <UserAvatarIcon {...props} />,
+  //  // Replaces the default bot chat message container
+  //  botChatMessage: (props) => <MyCustomChatMessage {...props} />,
+  
+  //  // Replaces the default user chat message
+  //  userChatMessage: (props) => <MyCustomUserChatMessage {...props} />
+ },
+
 };
 
 export default config;
---
+++ frontEnd/chatbot/src/UI/UserAvatar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./UserAvatar.css"
+import UserAvatar from "../Icons/userAvatar.svg";
+
+const UserAvatarIcon = () =>{
+    return (
+        <div className="react-chatbot-kit-user-avatar-icon">
+            <div className="react-chatbot-kit-user-avatar-container" style={{ background: "none" }}>
+                <img className = "UserAvatar" src={UserAvatar} />
+            </div>
+        </div>
+    )
+}
+
+export default UserAvatarIcon;
