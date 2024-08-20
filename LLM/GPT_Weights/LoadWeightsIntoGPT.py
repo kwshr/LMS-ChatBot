a9a16eadc064471da566dd7cb62713ec1b369a6f
Kawshar <kwshr96626@gmail.com>
2024-08-20 15:31:49 -0400

    Cite author

--- LLM/GPT_Weights/LoadWeightsIntoGPT.py
+++ LLM/GPT_Weights/LoadWeightsIntoGPT.py
@@ -1,3 +1,8 @@
+# Copyright (c) Sebastian Raschka under Apache License 2.0 (see LICENSE.txt).
+# Source for "Build a Large Language Model From Scratch"
+#   - https://www.manning.com/books/build-a-large-language-model-from-scratch
+# Code: https://github.com/rasbt/LLMs-from-scratch
+
 import numpy as np
 from GPT_Weights.AssignWeigths import assign
 from GPT_Weights.GPTInstance import gpt
--- LLM/GPT_Weights/Main.py
+++ LLM/GPT_Weights/Main.py
@@ -1,3 +1,8 @@
+# Copyright (c) Sebastian Raschka under Apache License 2.0 (see LICENSE.txt).
+# Source for "Build a Large Language Model From Scratch"
+#   - https://www.manning.com/books/build-a-large-language-model-from-scratch
+# Code: https://github.com/rasbt/LLMs-from-scratch
+# 
 from gpt_download import download_and_load_gpt2
 
 settings, params = download_and_load_gpt2(model_size="124M", models_dir="gpt2")
